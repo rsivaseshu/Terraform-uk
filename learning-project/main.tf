ec632cf5df785fcb426192766a7480d3694551f6
rsivaseshu <rsivaseshu@gmail.com>
2023-08-03 12:53:17 +0530

    backend config update

--- learning-project/main.tf
+++ learning-project/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "rssr-uk-tf-backend-1"
+    key    = "terraform/uk/s3"
+    region = "eu-west-2"
+    profile = "some"
+  }
 }
 
 provider "aws" {
